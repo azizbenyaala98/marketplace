60f8d455ff4470c7388f6fb61994629af58780ac
Baha chammakhi <baha@vikonnekt.com>
2023-12-17 19:26:58 +0100

    Add cancel button to edit product page

--- src/app/edit-product/edit-product.page.ts
+++ src/app/edit-product/edit-product.page.ts
@@ -1,36 +1,62 @@
-
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {
+  Storage,
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytes,
+} from '@angular/fire/storage';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
 import { Product, ProductCategory } from '../models/product';
 
+/**
+ * Represents the EditProductPage component.
+ */
 @Component({
   selector: 'app-edit-product',
   templateUrl: './edit-product.page.html',
   styleUrls: ['./edit-product.page.scss'],
 })
 export class EditProductPage implements OnInit {
+  storage: Storage;
+  ref: typeof ref;
   productId: string;
+  uploadCompleted = true;
   product: Product = {
     title: '',
     price: 0,
     description: '',
     category: ProductCategory.Sell, // Set the default category
     imageUrl: '',
+    phone: 0,
   };
   productImage: string | ArrayBuffer;
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private productService: ProductService
   ) {}
 
+  /**
+   * Initializes the component.
+   */
   ngOnInit() {
+    this.storage = getStorage();
+    this.ref = ref;
     this.route.params.subscribe((params) => {
       this.productId = params['id'];
-      
+      this.productService.getProduct(params['id']).then((product) => {
+        this.product = product;
+      });
     });
   }
+
+  /**
+   * Handles the file change event.
+   * @param event The file change event.
+   */
   onFileChange(event) {
     const reader = new FileReader();
 
@@ -44,11 +70,44 @@ export class EditProductPage implements OnInit {
       };
     }
   }
-  EditProduct(){
-    this.productService.updateProduct(this.product)
-    
+
+  /**
+   * Navigates to the dashboard page.
+   */
+  navigate() {
+    this.router.navigateByUrl(`/dashboard`);
   }
 
- 
+  /**
+   * Uploads a file.
+   * @param event The file upload event.
+   */
+  uploadFile(event: any) {
+    this.uploadCompleted = false;
+    const file = event.target.files[0];
+    const fileName = file.name; // Get the file name
+    const filePath = fileName + new Date().toISOString();
+    const ref = this.ref(this.storage, filePath);
+    uploadBytes(ref, file).then((snapshot) => {
+      console.log('Uploaded a blob or file!');
+      getDownloadURL(ref).then((downloadUrl) => {
+        this.productImage = downloadUrl;
+        this.product.imageUrl = downloadUrl;
+        this.uploadCompleted = true;
+      });
+    });
+  }
 
+  /**
+   * Updates the product.
+   */
+  EditProduct() {
+    if (!this.uploadCompleted) {
+      alert('Please upload first');
+      return;
+    }
+    this.productService.updateProduct(this.product).then(() => {
+      this.router.navigateByUrl(`/dashboard`);
+    });
+  }
 }
--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,15 +1,42 @@
 export enum ProductCategory {
-    Rent = 'rent',
-    Sell = 'sell'
-  }
-  
-  export interface Product {
-    id?: string;
-    title: string;
-    category: ProductCategory; 
-    price: number;
-    description: string;
-    imageUrl: string;
-    userId?: string;
-    
-  }
+  Rent = 'rent',
+  Sell = 'sell',
+}
+
+/**
+ * Represents a product.
+ */
+export interface Product {
+  /**
+   * The unique identifier of the product.
+   */
+  id?: string;
+  /**
+   * The title of the product.
+   */
+  title: string;
+  /**
+   * The category of the product.
+   */
+  category: ProductCategory;
+  /**
+   * The price of the product.
+   */
+  price: number;
+  /**
+   * The description of the product.
+   */
+  description: string;
+  /**
+   * The URL of the product's image.
+   */
+  imageUrl: string;
+  /**
+   * The unique identifier of the user who owns the product.
+   */
+  userId?: string;
+  /**
+   * The phone number associated with the product.
+   */
+  phone: number;
+}
