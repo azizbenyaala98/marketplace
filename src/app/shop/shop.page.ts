ac033927c6134cdc86eac052856448a93a048794
Baha chammakhi <baha@vikonnekt.com>
2023-12-17 19:27:19 +0100

    Delete unused image upload service and update shop page

--- src/app/shop/shop.page.ts
+++ src/app/shop/shop.page.ts
@@ -4,34 +4,56 @@ import { Observable, Subscription } from 'rxjs';
 import { Product } from '../models/product';
 import { ProductService } from '../services/product.service';
 
+/**
+ * Represents the ShopPage component.
+ */
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.page.html',
   styleUrls: ['./shop.page.scss'],
 })
 export class ShopPage implements OnInit {
-
+  /**
+   * The list of products.
+   */
   productList: Product[];
+
+  /**
+   * The subscription to the product list.
+   */
   productListSubscription: Subscription;
 
-
+  /**
+   * Initializes the component.
+   */
   ngOnInit() {
-    this.productListSubscription = this.productService.getProducts().subscribe((products: Product[]) => {
-      this.productList = products;
-    });
+    this.productListSubscription = this.productService
+      .getProducts()
+      .subscribe((products: Product[]) => {
+        this.productList = products;
+      });
   }
 
-  constructor(private router: Router,
-    private productService :ProductService) {}
+  /**
+   * Constructs a new instance of the ShopPage component.
+   * @param router - The router service.
+   * @param productService - The product service.
+   */
+  constructor(private router: Router, private productService: ProductService) {}
 
+  /**
+   * Navigates to the sign up page.
+   */
   goToSignUp() {
     // Define the action for Sign Up button
     this.router.navigate(['/signup']);
   }
 
+  /**
+   * Navigates to the sign in page.
+   */
   goToSignIn() {
     // Define the action for Sign In button
     this.router.navigate(['/login']);
   }
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,19 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+/**
+ * Represents the environment configuration for the application.
+ */
 export const environment = {
   production: false,
-  firebaseConfig :{
-    apiKey: "AIzaSyApv8rpDYo3PKWIsXAtRbtHB-OfabMkAvI",
-    authDomain: "ionic-9f496.firebaseapp.com",
-    projectId: "ionic-9f496",
-    storageBucket: "ionic-9f496.appspot.com",
-    messagingSenderId: "279399281863",
-    appId: "1:279399281863:web:2e6b05ff7a54e9b3723ae2"
-  }
+  firebaseConfig: {
+    apiKey: 'AIzaSyApv8rpDYo3PKWIsXAtRbtHB-OfabMkAvI',
+    authDomain: 'ionic-9f496.firebaseapp.com',
+    projectId: 'ionic-9f496',
+    storageBucket: 'ionic-9f496.appspot.com',
+    messagingSenderId: '279399281863',
+    appId: '1:279399281863:web:2e6b05ff7a54e9b3723ae2',
+  },
 };
 
 /*
